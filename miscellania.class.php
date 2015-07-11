179fbd7e472097d552bed626839342b8ae851684
Versionamento11 <marciacamelo2003@yahoo.com>
2015-07-11 16:04:02 -0300

    Numero Perfeito Felipe Camelo e Felipe Melo

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,3 +1,37 @@
 class Miscellania {
-agora vai/tia
+<?php
+   function pokopikaDiv($nu){
+ $divisores = array();
+    $metInf = floor($nu/2);
+  for($indio=1;$i<=$metInf;$indio++){
+   if(!($nu%$i))
+    $divisores[] = $indio;
+  }
+ return $divisores;
+}
+
+              function somaPerfeita($nu,$div){
+ $soma = 0;
+ foreach($div as $divisores){
+  $soma += $divisores;
+ } 
+ if($soma==$nu){
+  return true;
+ }else
+ {
+  return false;
+ }
+}
+$nu = $_GET["n"];
+$numervrauPerf = array();
+$i = 2; 
+
+while(count($numervrauPerf)<$nu){
+        $div = retornaDiv($indio);
+ if(somaPerfeita($indio,$div))
+      $numervrauPerf[] = $indio;
+ $indio += 2;
+}
+
+?>
 }
